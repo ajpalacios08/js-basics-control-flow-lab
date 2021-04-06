0c9dac306dfdec76a671a36c9a22afd5c1ba2fbd
Allen <apalacios1994@gmail.com>
2021-04-06 13:21:33 -0500

    commit tree 14d5cc7b4d2b0a55b41da2b6d31424ae5b225c33

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberFeet){
+  if (numberFeet <= 400){
+    return 'This one is on me!';
+  }else if (numberFeet > 2000, numberFeet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else if (numberFeet > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityTrip){
+  const eitherWay = cityTrip === 'NYC' ? "Ok, sounds good." : "No go.";
+  return eitherWay;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let responseToTip;
+  switch (tip) {
+    case tip === 'generous':
+      responseToTip = "Thank you so much.";
+      break;
+
+    case tip === 'not as generous':
+      responseToTip = "Thank you.";
+      break;
+
+    case tip === '':
+      responseToTip = "Bye.";
+      break;
+  }
+  return responseToTip;
 }
